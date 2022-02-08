a2ec70451646845ab78bddaec2bc5bc04f6e6389
DrInnovatio <turing1723@gmail.com>
2022-02-08 20:21:12 +0900

    the second commit.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>hey</p>
+  <greeting></greeting>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import Greeting from "./components/Greeting.vue"
+  export default {
+    name: "App",
+    components: {
+      Greeting,
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style> 
+
 </style>
---
+++ src/components/Greeting.vue
@@ -0,0 +1,28 @@
+/* eslint-disable vue/multi-word-component-names */
+<template>
+  <p>{{msg}}</p>
+</template>
+
+<script>
+
+  export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: "Greeting",
+    data(){
+      return {
+        msg: "Kiil Putin."
+      };
+    }
+  }
+
+</script>
+ 
+<style scoped lang="scss">
+  $color: rgb(0, 255, 102);
+
+  p {
+    color: $color;
+    text-align: center;
+    background-color: blueviolet;
+  }
+</style>
