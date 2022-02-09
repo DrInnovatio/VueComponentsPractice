ea3af23953ac66a0c93632c44db9f9b81445f266
DrInnovatio <turing1723@gmail.com>
2022-02-09 22:02:54 +0900

    added User.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <p>hey</p>
+  <h3>hey</h3>
   <greeting></greeting>
+  <user></user>
 </template>
 
 <script>
 
 import Greeting from "./components/Greeting.vue"
+import User from "./components/User.vue"
+
   export default {
     name: "App",
     components: {
       Greeting,
+      User,
     }
   }
 </script>
---
+++ src/components/User.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>The user is x years old.</p>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "User"
+}
+</script>
+
+<style>
+
+</style>
