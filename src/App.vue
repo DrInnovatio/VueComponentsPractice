02d8d5922a0c7188ae5bce079f03273d8d30f749
DrInnovatio <turing1723@gmail.com>
2022-02-13 23:19:49 +0900

    Callback Function.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <h3>hey</h3>
   <button type="button" @click="age++">Update Age</button>
   <greeting :age="age"></greeting>
-  <user :age="age" @age-change="updateAge" :firstName="firstName"></user>
+  <user :age="age" @age-change="updateAge" :ageChangeFn="updateAgeCB" :firstName="firstName"></user>
 </template>
 
 <script>
@@ -25,9 +25,11 @@ import User from "./components/User.vue"
     methods: {
       updateAge(num){
         this.age += num;
+      },
+      updateAgeCB(num){
+        this.age += num;
       }
     }
-
   }
 </script>
 
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,5 +1,6 @@
 <template>
   <button type="button" @click="onClickAge">Update Age</button>
+  <button type="button" @click="ageChangeFn(3)">Update Age CB</button>
   <p>The {{firstName}} is {{age}} years old.</p>
   <p>{{ageDoubled}}</p>
 </template>
@@ -14,10 +15,11 @@ export default {
       //required: true,
       //default: 20
       validator(value){
-        this.onClickAge();
+        //this.onClickAge();
         return value < 130
       }
     },
+    ageChangeFn: Function,
     firstName: {
       type: String,
       //required: true
